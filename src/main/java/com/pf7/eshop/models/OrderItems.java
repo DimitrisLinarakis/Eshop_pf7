9d640e0f30fd4fbc32a4f35acb324043be468d67
ParaskeviMega <vivi-mega1@hotmail.com>
2021-12-03 20:01:15 +0200

    models package

---
+++ src/main/java/com/pf7/eshop/models/OrderItems.java
@@ -0,0 +1,10 @@
+package com.pf7.eshop.models;
+
+public class OrderItems {
+    private Integer orderItemsID;
+    private Integer productID;
+    private Integer orderId;
+    private Integer quantity;
+
+
+}
