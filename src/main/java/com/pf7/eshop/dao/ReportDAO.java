ace7f7a52aa7adf455e05c65c681db95dfd7837b
Manos Fragkiadakis <fragomanos1@hotmail.com>
2021-12-09 13:16:10 +0200

    Fix Reports Queries If has rows

--- src/main/java/com/pf7/eshop/dao/ReportDAO.java
+++ src/main/java/com/pf7/eshop/dao/ReportDAO.java
@@ -1,7 +1,6 @@
 package com.pf7.eshop.dao;
 
 import com.pf7.eshop.controller.DatabaseController;
-import com.pf7.eshop.model.PaymentMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,17 +19,17 @@ public class ReportDAO {
 
     public void getPreviewReportPerParticularCustomer(int CustomerId) {
         try {
-            ResultSet resultSet = statement.executeQuery("select C.NAME,C.SURNAME,count(*) as TotalCount,Round(sum(TOTALPRICE),2) as Total from ORDERS ord " +
+            ResultSet resultSet = statement.executeQuery("select C.NAME as Name,C.SURNAME as Surname,count(*) as TotalCount,Round(sum(TOTALPRICE),2) as Total from ORDERS ord " +
                     "inner join CUSTOMERS C on ord.CUSTOMERID = C.CUSTOMERID " +
                     "where ord.CUSTOMERID = '" + CustomerId + "'" +
                     "group by C.NAME, C.SURNAME " +
                     "order BY Total DESC;");
 
-            if (resultSet.next()) {
+            if (resultSet.getFetchSize() > 0) {
                 while (resultSet.next()) {
                     logger.info("Total And Cost Per Particular Customer -> Name : {}, Surname:{}, Total Number :{}, Cost Of Purchases : {}",
-                            resultSet.getString("NAME"),
-                            resultSet.getString("SURNAME"),
+                            resultSet.getString("Name"),
+                            resultSet.getString("Surname"),
                             resultSet.getInt("TotalCount"),
                             resultSet.getBigDecimal("Total"));
 
@@ -46,15 +45,15 @@ public class ReportDAO {
 
     public void getPreviewReportPerCustomerCategory() {
         try {
-            ResultSet resultSet = statement.executeQuery("select C.CUSTOMERCATEGORY,count(*) as TotalCount,Round(sum(TOTALPRICE),2) as Total from ORDERS " +
+            ResultSet resultSet = statement.executeQuery("select C.CUSTOMERCATEGORY as Category,count(*) as TotalCount,Round(sum(TOTALPRICE),2) as Total from ORDERS " +
                     "inner join CUSTOMERS C on ORDERS.CUSTOMERID = C.CUSTOMERID " +
                     "group BY C.CUSTOMERCATEGORY " +
                     "order by Total DESC;");
 
-            if (resultSet.next()) {
+            if (resultSet.getFetchSize() > 0) {
                 while (resultSet.next()) {
                     logger.info("Category {} -> Total Number :{}, Cost Of Purchases : {} ",
-                            resultSet.getString("CustomerCategory"),
+                            resultSet.getString("Category"),
                             resultSet.getInt("TotalCount"),
                             resultSet.getBigDecimal("Total"));
 
@@ -70,14 +69,14 @@ public class ReportDAO {
 
     public void getPreviewReportPerPaymentMethod() {
         try {
-            ResultSet resultSet = statement.executeQuery("select ord.PAYMENTMETHOD,count(*) as TotalCount,Round(sum(TOTALPRICE),2) as Total from ORDERS ord " +
+            ResultSet resultSet = statement.executeQuery("select ord.PAYMENTMETHOD as Method,count(*) as TotalCount,Round(sum(TOTALPRICE),2) as Total from ORDERS ord " +
                     "GROUP BY ord.PAYMENTMETHOD " +
                     "order by Total DESC;");
 
-            if (resultSet.next()) {
+            if (resultSet.getFetchSize() > 0) {
                 while (resultSet.next()) {
                     logger.info("Payment Method {} -> Total Number :{}, Cost Of Purchases : {} ",
-                            resultSet.getString("PaymentMethod"),
+                            resultSet.getString("Method"),
                             resultSet.getInt("TotalCount"),
                             resultSet.getBigDecimal("Total"));
 
@@ -101,7 +100,7 @@ public class ReportDAO {
                     "group by pr.PRICE,pr.PRODUCTID,C.CUSTOMERID,pr.NAME " +
                     "order by PRICE DESC;");
 
-            if (resultSet.next()) {
+            if (resultSet.getFetchSize() > 0) {
                 while (resultSet.next()) {
                     logger.info("Most Expensive Products ->Customer Id : {} , Name : {} , Surname : {} , Product Name : {} , Price : {}, Products Purchases : {} ",
                             resultSet.getInt("CustomerId"),
